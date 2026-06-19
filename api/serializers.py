13b8f7e1c12e0cb5bf031a9ce2781dd1facb650d
LumbaBalumba <vlad.lazar.ik528@yandex.ru>
2026-06-19 11:08:39 +0300

    fix viewset

--- api/serializers.py
+++ api/serializers.py
@@ -6,15 +6,17 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = "__all__"
-        depth = 2
-
-
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
-        fields = "__all__"
-        depth = 2
+        fields = ("id", "name")
+        read_only_fields = fields
+
+
+class UserSerializer(serializers.ModelSerializer):
+    groups = GroupSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ("id", "username", "email", "first_name", "last_name", "groups")
+        read_only_fields = fields
--- api/views.py
+++ api/views.py
@@ -1,10 +1,10 @@
-from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from api.serializers import GroupSerializer, User, UserSerializer
 
 
-class UserViewSet(ModelViewSet):
+class UserViewSet(ReadOnlyModelViewSet):
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
 
@@ -12,9 +12,9 @@ class UserViewSet(ModelViewSet):
         return User.objects.filter(id=self.request.user.id)
 
 
-class GroupViewSet(ModelViewSet):
+class GroupViewSet(ReadOnlyModelViewSet):
     serializer_class = GroupSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        return self.request.user.groups
+        return self.request.user.groups.all()
